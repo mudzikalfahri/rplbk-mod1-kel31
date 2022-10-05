22053184aa0e494deb6dce17d1dcf44396b65b0a
mudzikalfahri <alfahrimudzik@gmail.com>
2022-10-06 06:28:14 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    nama_lengkap: "Yohanes Kukuh Niti Cahya Bagaskara",
+    nama_panggilan: "Bagas",
+    nomor_telepon: "081393566854",
+    id_line: "bagaskukuh_",
+    tanggal_lahir: "8/30/2001",
+    nim: 21120119130095,
+    email: "bagaskukuh2@gmail.com",
+    hobi: "baca buku",
+  },
+  {
+    nama_lengkap: "Satria Bimantara Dharmawan",
+    nama_panggilan: "Bima",
+    nomor_telepon: "081328694344",
+    id_line: "sabimantaradh",
+    tanggal_lahir: "7/14/2001",
+    nim: 21120119130096,
+    email: "bimantaradharmawan@gmail.com",
+    hobi: "Billiard",
+  },
+];
